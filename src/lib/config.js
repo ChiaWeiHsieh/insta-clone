5fa0fd8545ab1057706267e6d51dac56a424579c
Weigo <s2310602031@gms.nutc.edu.tw>
2021-04-23 14:34:53 +0800

    add config

---
+++ src/lib/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCxvaLDX2bYjVOIBqjI4Y59vryuJeitoxg",
+    authDomain: "insta-clone-52223.firebaseapp.com",
+    projectId: "insta-clone-52223",
+    storageBucket: "insta-clone-52223.appspot.com",
+    messagingSenderId: "378657926122",
+    appId: "1:378657926122:web:fbd4df6f1f84f206736093"
+}
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,10 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import config from './config';
 // import { seedDatabase } from '../seed';
 
-const config = {
-    apiKey: "AIzaSyCxvaLDX2bYjVOIBqjI4Y59vryuJeitoxg",
-    authDomain: "insta-clone-52223.firebaseapp.com",
-    projectId: "insta-clone-52223",
-    storageBucket: "insta-clone-52223.appspot.com",
-    messagingSenderId: "378657926122",
-    appId: "1:378657926122:web:fbd4df6f1f84f206736093"
-};
+// choice yout api key
 
 const firebase = Firebase.initializeApp(config);
 
